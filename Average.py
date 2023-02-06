0d63e97762b4103348831838d4ba442334a8b5a4
johngichuhi218 <123934571+johngichuhi218@users.noreply.github.com>
2023-02-06 16:07:04 +0300

    average score for three units

---
+++ Average.py
@@ -0,0 +1,6 @@
+#program to calculate the average grade and grade for three units
+unit1 = int(input("Enter the score for the first unit: "))
+unit2 = int(input("Enter the score for the second unit: "))
+unit3 = int(input("Enter the score for the third unit: "))
+score = ((unit1 + unit2 + unit3)/3)
+print("The average score is: " ,score)
