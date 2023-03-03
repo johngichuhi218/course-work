6d9ea2abaf3cd67e15e8ca3ce1e379601b668eb2
johngichuhi218 <123934571+johngichuhi218@users.noreply.github.com>
2023-03-03 18:06:29 +0300

    convert celsius to fahrenheit

---
+++ convertion.py
@@ -0,0 +1,11 @@
+class Temperature:
+    def _init_(self, Fahrenhiet, Celsius):
+        self.Fahrenhiet = Fahrenhiet
+        self.Celsius = Celsius
+    def Fahrenhiet(self)
+      return(self.Fahrenheit*(9/5)+32)
+    def Celsius(self)
+      return(self.Celsius*(9/5)-32)
+Temp = Temperature(30, 50)
+print(temp.Fahrenheit())
+print(temp.Celsius())
